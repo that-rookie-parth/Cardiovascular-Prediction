dabfe73fac04ee73ff77ce345d905d7f70544db8
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-27 15:27:13 +0530

    yarn create vite client

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import "./main.scss";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.scss";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <>
+        <App />
+    </>
+);
