49d549795c497b5d5e205ac8b6722e95aeb92ca6
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-29 11:40:34 +0530

    moved the code uner a if blcok

--- app.py
+++ app.py
@@ -2,24 +2,6 @@ import pickle as pkl
 import streamlit as st
 import numpy as np
 
-cholestrol_choices = ['Normal', 'Above Normal', 'Well Above Normal']
-active_choices = ["Yes", "No"]
-
-st.title('Cardiovascular Disease Prediction :anatomical_heart:')
-height = st.number_input("Enter your height ( in cms )")
-sbp = st.number_input("Enter your Systolic Blood Pressure ( in mmHg )")
-dbp = st.number_input("Enter your Diastolic Blood Pressure ( in mmHg )")
-
-cholestrol = st.selectbox(
-    'Select your cholestron level', cholestrol_choices)
-
-active = st.selectbox(
-    'Are you physically active?', active_choices)
-
-age_y = st.number_input("Enter your age ( in Years )")
-pulse = st.number_input("Enter your pulse ( in pulse/sec )")
-
-
 with open("./model.pkl", "rb") as file:
     model = pkl.load(file)
 
@@ -44,4 +26,22 @@ def predict():
             "Congratulations! According to us, you don't have the disease. :heart_decoration:")
 
 
-trigger = st.button("Predict", on_click=predict)
+if not hasattr(st, 'already_started_server'):
+
+    cholestrol_choices = ['Normal', 'Above Normal', 'Well Above Normal']
+    active_choices = ["Yes", "No"]
+
+    st.title('Cardiovascular Disease Prediction :anatomical_heart:')
+    height = st.number_input("Enter your height ( in cms )")
+    sbp = st.number_input("Enter your Systolic Blood Pressure ( in mmHg )")
+    dbp = st.number_input("Enter your Diastolic Blood Pressure ( in mmHg )")
+
+    cholestrol = st.selectbox(
+        'Select your cholestron level', cholestrol_choices)
+
+    active = st.selectbox(
+        'Are you physically active?', active_choices)
+
+    age_y = st.number_input("Enter your age ( in Years )")
+    pulse = st.number_input("Enter your pulse ( in pulse/sec )")
+    trigger = st.button("Predict", on_click=predict)
